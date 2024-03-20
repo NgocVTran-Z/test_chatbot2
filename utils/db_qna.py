d97dfa60fe8c07bd63d21e93f7fcc512bdcbbc99
Ngoc V. Tran <113083475+NgocVTran-Z@users.noreply.github.com>
2024-03-20 22:18:45 +0700

    Add files via upload

---
+++ utils/db_qna.py
@@ -0,0 +1,8 @@
+db_ans = (
+    ("cho tôi biết doanh thu tháng 2", "Doanh thu tháng 2 là 1,099,000 VND"),
+    ("những mặt hàng nào bán chạy nhất?", "Mặt hàng bán chạy nhất là Dry-ex áo polo ngắn tay"),
+    ("tháng 2 bán được bao nhiêu cái Dry-ex polo?", "Có tổng cộng 2 cái được bán trong tháng 2"),
+    ("Dry-ex polo trong kho còn bao nhiêu cái?", "Dry-ex áo polo ngắn tay tại cửa hàng Quận 1 còn một cái, "
+                                                 "tại cửa hàng Quận 3 còn 20 cái"),
+    ("có thể ship hàng từ Q3 sang Q1 ko?", "Có, chi phí ship là 20,000 cho 1km, chi phí dự kiến là 85,000"),
+)
---
+++ utils/qna.py
@@ -0,0 +1,9 @@
+ans = (
+    ("tôi muốn mua áo thun", "Bạn vui lòng cho biết kích cỡ và màu áo bạn thích?"),
+    ("có size M màu trắng ko?", "Có, Áo Thun Dry-EX Cổ Tròn Ngắn Tay và AIRism Cotton Áo Thun Kẻ Sọc Cổ Tròn Dáng Rộng "
+                                "đều có size M màu trắng của nam, hiện đang bán tại cửa hàng Quận 1 và Quận 3"),
+    ("cửa hàng Q1 địa chỉ là gì?", "Cửa hàng Quận 1 địa chỉ 17 Lê Thánh Tông"),
+    ("có hot line ko?", "Cửa hàng Quận 1 có số điện thoại là 0901 234 567 89"),
+    ("cửa hàng hiện có đang mở cửa ko?", "Giờ mở cửa của cửa hàng Quận 1 là từ 8h sáng đến 10h đêm thứ 2-7 hàng tuần"),
+    ("cảm ơn", "Cảm ơn quý khách đã ghé thăm")
+)
