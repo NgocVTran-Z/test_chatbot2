85be5e77d0828eba14bf23993bba1b4181fa3e0e
Ngoc V. Tran <113083475+NgocVTran-Z@users.noreply.github.com>
2024-03-21 06:40:24 +0700

    Update db_qna.py

--- utils/db_qna.py
+++ utils/db_qna.py
@@ -1,8 +1,10 @@
 db_ans = (
-    ("cho tôi biết doanh thu tháng 2", "Doanh thu tháng 2 là 1,099,000 VND"),
-    ("những mặt hàng nào bán chạy nhất?", "Mặt hàng bán chạy nhất là Dry-ex áo polo ngắn tay"),
-    ("tháng 2 bán được bao nhiêu cái Dry-ex polo?", "Có tổng cộng 2 cái được bán trong tháng 2"),
+    ("tổng doanh thu tháng 3 hiện tại đang là bao nhiêu?", "tổng doanh thu hiện tại đang là 316700400"),
+    ("cửa hàng nào đang có doanh thu cao nhất?", "cửa hàng Quận 1 đang có doanh thu cao nhất"),
+    ("mặt hàng nào đang được bán nhiều nhất?", "áo thun Dry-Ex polo ngắn tay đang được bán nhiều "
+                                               "nhất ở cửa hàng Quận 1"),
     ("Dry-ex polo trong kho còn bao nhiêu cái?", "Dry-ex áo polo ngắn tay tại cửa hàng Quận 1 còn một cái, "
                                                  "tại cửa hàng Quận 3 còn 20 cái"),
     ("có thể ship hàng từ Q3 sang Q1 ko?", "Có, chi phí ship là 20,000 cho 1km, chi phí dự kiến là 85,000"),
+    ("có cần bổ sung thêm hàng hay không?", "cần bổ sung thêm mặt hàng Dry-ex polo cho cửa hàng Quận 1"),
 )
